658780b057db5534413236ff5ac39a613ef4bf58
uguumur <baterdeneorgil22@gmail.com>
2023-02-03 16:37:17 +0800

    footer change

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Footer() {
-  return <div className="w-full h-40 bg-primary text-white ">Footer</div>;
+  return <div className="w-full h-40 bg-primary text-white ">Footer v2</div>;
 }
