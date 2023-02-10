02909fc61eb7a8788d4298fc1e9a7162b11f2023
uguumur <uguumur.oldokh@gmail.com>
2023-02-10 11:35:32 +0800

    added pay.js

---
+++ src/components/pay.js
@@ -0,0 +1,182 @@
+import React from "react";
+export default function Pay() {
+  return (
+    <>
+      <div className="w-full h-[32rem] bg-white rounded-3xl shadow-md mt-28 mb-20">
+        <div className="flex justify-center pt-10 font-bold text-2xl font-[Ubuntu]">
+          <div>Үйлчилгээний төлбөр</div>
+        </div>
+        <div className="flex justify-evenly">
+          <div className="flex pt-12">
+            <div className="w-96 h-44  flex flex-col items-center pt-4 rounded-2xl border-2 border-gray">
+              <div className="font-[Ubuntu] font-bold pb-2">Junior</div>
+              <div className="w-80 h-0.5 rounded-lg bg-secondary "></div>
+              <div className="flex gap-4 pt-2">
+                <div className="w-24 h-10 flex flex-col items-center">
+                  <div className="font-[Ubuntu] text-sm font-bold pb-2">
+                    6 сар
+                  </div>
+                  <div className="w-full h-0.5 bg-black rounded-xl"></div>
+                </div>
+                <div className="w-20 h-10 flex flex-col items-center">
+                  <div className="font-[Ubuntu] text-sm font-bold pb-2">
+                    1 жил
+                  </div>
+                  <div className="w-full h-0.5 bg-black rounded-xl"></div>
+                </div>
+                <div className="w-24 h-10 flex flex-col items-center">
+                  <div className="font-[Ubuntu] text-sm font-bold pb-2">
+                    12 сар
+                  </div>
+                  <div className="w-full h-0.5 bg-black rounded-xl"></div>
+                </div>
+              </div>
+              <div className="flex gap-2 pt-2">
+                <div>
+                  <div className="w-24 h-10 flex flex-col items-center">
+                    <div className="font-[Ubuntu] text-sm font-bold pb-2">
+                      2 600 000 ₮
+                    </div>
+                    <div className="w-24 h-0.5 bg-black rounded-xl"></div>
+                  </div>{" "}
+                </div>
+                <div>
+                  <div className="w-24 h-10 flex flex-col items-center">
+                    <div className="font-[Ubuntu] text-sm font-bold pb-2">
+                      2 600 000 ₮
+                    </div>
+                    <div className="w-24 h-0.5 bg-black rounded-xl"></div>
+                  </div>
+                </div>
+                <div>
+                  <div className="w-24 h-10 flex flex-col items-center">
+                    <div className="font-[Ubuntu] text-sm font-bold pb-2">
+                      2 600 000 ₮
+                    </div>
+                    <div className="w-24 h-0.5 bg-black rounded-xl"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex pt-12">
+            <div className="w-96 h-44  flex flex-col items-center pt-4 rounded-2xl border-2 border-gray">
+              <div className="font-[Ubuntu] font-bold pb-2">Junior</div>
+              <div className="w-80 h-0.5 rounded-lg bg-secondary "></div>
+              <div className="flex gap-4 pt-2">
+                <div className="w-24 h-10 flex flex-col items-center">
+                  <div className="font-[Ubuntu] text-sm font-bold pb-2">
+                    6 сар
+                  </div>
+                  <div className="w-full h-0.5 bg-black rounded-xl"></div>
+                </div>
+                <div className="w-20 h-10 flex flex-col items-center">
+                  <div className="font-[Ubuntu] text-sm font-bold pb-2">
+                    1 жил
+                  </div>
+                  <div className="w-full h-0.5 bg-black rounded-xl"></div>
+                </div>
+                <div className="w-24 h-10 flex flex-col items-center">
+                  <div className="font-[Ubuntu] text-sm font-bold pb-2">
+                    12 сар
+                  </div>
+                  <div className="w-full h-0.5 bg-black rounded-xl"></div>
+                </div>
+              </div>
+              <div className="flex gap-2 pt-2">
+                <div>
+                  <div className="w-24 h-10 flex flex-col items-center">
+                    <div className="font-[Ubuntu] text-sm font-bold pb-2">
+                      2 600 000 ₮
+                    </div>
+                    <div className="w-24 h-0.5 bg-black rounded-xl"></div>
+                  </div>{" "}
+                </div>
+                <div>
+                  <div className="w-24 h-10 flex flex-col items-center">
+                    <div className="font-[Ubuntu] text-sm font-bold pb-2">
+                      2 600 000 ₮
+                    </div>
+                    <div className="w-24 h-0.5 bg-black rounded-xl"></div>
+                  </div>
+                </div>
+                <div>
+                  <div className="w-24 h-10 flex flex-col items-center">
+                    <div className="font-[Ubuntu] text-sm font-bold pb-2">
+                      2 600 000 ₮
+                    </div>
+                    <div className="w-24 h-0.5 bg-black rounded-xl"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex pt-12 drop-shadow-xl">
+            <div className="w-96 h-44  flex flex-col items-center pt-4 rounded-2xl border-2 border-gray">
+              <div className="font-[Ubuntu] font-bold pb-2">Junior</div>
+              <div className="w-80 h-0.5 rounded-lg bg-secondary "></div>
+              <div className="flex gap-4 pt-2">
+                <div className="w-24 h-10 flex flex-col items-center">
+                  <div className="font-[Ubuntu] text-sm font-bold pb-2">
+                    6 сар
+                  </div>
+                  <div className="w-full h-0.5 bg-black rounded-xl"></div>
+                </div>
+                <div className="w-20 h-10 flex flex-col items-center">
+                  <div className="font-[Ubuntu] text-sm font-bold pb-2">
+                    1 жил
+                  </div>
+                  <div className="w-full h-0.5 bg-black rounded-xl"></div>
+                </div>
+                <div className="w-24 h-10 flex flex-col items-center">
+                  <div className="font-[Ubuntu] text-sm font-bold pb-2">
+                    12 сар
+                  </div>
+                  <div className="w-full h-0.5 bg-black rounded-xl"></div>
+                </div>
+              </div>
+              <div className="flex gap-2 pt-2">
+                <div>
+                  <div className="w-24 h-10 flex flex-col items-center">
+                    <div className="font-[Ubuntu] text-sm font-bold pb-2">
+                      2 600 000 ₮
+                    </div>
+                    <div className="w-24 h-0.5 bg-black rounded-xl"></div>
+                  </div>{" "}
+                </div>
+                <div>
+                  <div className="w-24 h-10 flex flex-col items-center">
+                    <div className="font-[Ubuntu] text-sm font-bold pb-2">
+                      2 600 000 ₮
+                    </div>
+                    <div className="w-24 h-0.5 bg-black rounded-xl"></div>
+                  </div>
+                </div>
+                <div>
+                  <div className="w-24 h-10 flex flex-col items-center">
+                    <div className="font-[Ubuntu] text-sm font-bold pb-2">
+                      2 600 000 ₮
+                    </div>
+                    <div className="w-24 h-0.5 bg-black rounded-xl"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full h-20 flex justify-center text-center pt-8">
+          <div className="w-[47rem] h-20 text-center font-[Ubuntu] font-bold">
+            Тайлбар: Та дээрх үйлчилгээний төлбөрийг төлснөөр Таны профайл манай
+            хайлтын системд орж, хэрэлэгчдэд харагдах болно. Манай үйлчилгээ нь
+            шүүгч, прокуроруудад төлбөргүй байна.
+          </div>
+        </div>
+        <div className="w-full flex justify-center pt-12">
+          <button className="w-60 h-10 bg-[#EF233C] rounded-xl text-[#EDF2F4] font-[Ubuntu] font-semibold text-sm drop-shadow-md hover:drop-shadow-xl">
+            Бидэнтэй нэгд
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
