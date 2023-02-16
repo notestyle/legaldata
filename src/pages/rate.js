ccef54ea45368434c2dfd37d0d01010ce66ce955
uguumur <baterdeneorgil22@gmail.com>
2023-02-16 16:19:55 +0800

    Rate upload

---
+++ src/pages/rate.js
@@ -0,0 +1,10 @@
+import Layout from "@/components/layout";
+import RateMe from "@/components/rateMe";
+
+export default function SignIn() {
+  return (
+    <Layout>
+      <RateMe />
+    </Layout>
+  );
+}
