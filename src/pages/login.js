74f043135475a40fb682a4d25580c420da47be34
uguumur <baterdeneorgil22@gmail.com>
2023-02-27 14:55:55 +0800

    login input

--- src/pages/login.js
+++ src/pages/login.js
@@ -4,19 +4,19 @@ import Head from "next/head";
 import React from "react";
 
 export default function Login() {
-    return (
-        <>
-            <Head>
-                <title>Legal Data</title>
-                <meta name="description" content="Хуулийн зөвлөгөө" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/next.svg" />
-            </Head>
-            <Layout>
-                <div className="flex pt-20 justify-center">
-                    <Enter />
-                </div>
-            </Layout>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Legal Data</title>
+        <meta name="description" content="Хуулийн зөвлөгөө" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/next.svg" />
+      </Head>
+      <Layout>
+        <div className="flex pt-20 justify-center">
+          <Enter />
+        </div>
+      </Layout>
+    </>
+  );
 }
